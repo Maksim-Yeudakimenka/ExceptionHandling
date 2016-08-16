6fa795dfe771b269bb6d91e0a3adf5cc28757f75
Yeudakimenka-Maksim <evdokimenko.maxim@gmail.com>
2016-08-17 00:04:06 +0300

    Completed task

--- ExceptionHandling/Program.cs
+++ ExceptionHandling/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace ExceptionHandling
 {
@@ -10,6 +7,45 @@ namespace ExceptionHandling
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                string input = null;
+
+                try
+                {
+                    input = Console.ReadLine();
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Input string was too long.");
+                }
+                catch (OutOfMemoryException)
+                {
+                    Console.WriteLine("Not enough memory to allocate input string.");
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine($"Input error occured: {e.Message}.");
+                }
+
+                if (input == null)
+                {
+                    continue;
+                }
+
+                try
+                {
+                    Console.WriteLine(input[0]);
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("Nothing was entered.");
+                }
+                catch (IOException e)
+                {
+                    Console.WriteLine($"Output error occured: {e.Message}.");
+                }
+            }
         }
     }
 }
